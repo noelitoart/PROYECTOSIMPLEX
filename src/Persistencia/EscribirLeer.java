76b565a67395fe1900d60867c94c06e7bef763ba
Noe <noelitoart@gmail.com>
2017-07-24 07:56:50 -0500

    aplicamos la  persistencia para  poder  editar escribirLeer

--- src/Persistencia/EscribirLeer.java
+++ src/Persistencia/EscribirLeer.java
@@ -20,44 +20,5 @@ import javax.swing.JOptionPane;
  */
 public class EscribirLeer{
     
-    public void escribirFichero(String fichero, Object o){ 
-        try{
-           ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichero)); 
-           oos.writeObject(o);            
-           oos.close();
-        } catch (Exception e){
-           JOptionPane.showMessageDialog(null,"Ocurrio un error al escribir el primer dato del archio","Error!",JOptionPane.ERROR_MESSAGE);
-           e.printStackTrace();
-        }
-    }
-    public ArrayList  leeFichero(String fichero) throws IOException{
-        File archivoEntrada;
-        FileInputStream canalEntrada = null;
-        ObjectInputStream objetoEntrada = null;
-        ArrayList<Guardar> auxi= new ArrayList<Guardar>();
-        boolean fin = true;
-	try{
-		 archivoEntrada=new File(fichero);
-		canalEntrada = new FileInputStream(archivoEntrada);
-		 objetoEntrada = new ObjectInputStream(canalEntrada);
-		do{
-			try{
-			auxi.add((Guardar) objetoEntrada.readObject());
-			}catch(IOException | ClassNotFoundException exc){
-				fin=false;
-			}
-		}while (fin==true);
-		objetoEntrada.close();	
-		canalEntrada.close();
-//		archivoEntrada.delete();
-	}catch (IOException e) {
-//                objetoEntrada.close();	
-//		canalEntrada.close();
-		System.out.println("Error al extraer");
-//                 JOptionPane.showMessageDialog(null,"No se pudo cargar el archivo  ...");
-	}
-      
-        return auxi;
-        
-    } 
+    
 }
